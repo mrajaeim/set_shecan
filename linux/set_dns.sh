d15b025ac6010c5eae87274ce48abbb31c290b39
MRajaeiM <mrajaeim.pro@gmail.com>
2023-01-26 08:14:55 +0330

    [feat] add bash script

---
+++ linux/set_dns.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    # Use the current active ethernet adapter
+    adapter=$(ip route | awk '/^default/ { print $5 }')
+else
+    adapter=$1
+fi
+
+if [ "$2" = "-d" ]; then
+    # Reset DNS settings
+    nmcli con mod "$adapter" ipv4.dns-search ""
+    nmcli con mod "$adapter" ipv4.ignore-auto-dns yes
+    nmcli con mod "$adapter" ipv4.dns ""
+    nmcli con down "$adapter"
+    nmcli con up "$adapter"
+    echo "DNS settings have been reset"
+else
+    read -p "Is this the correct ethernet adapter: $adapter ? (yes/no)" confirm
+    if [ "$confirm" = "yes" ]; then
+        primary_dns=${3:-"178.22.122.100"}
+        secondary_dns=${4:-"185.51.200.2"}
+        nmcli con mod "$adapter" ipv4.dns "$primary_dns $secondary_dns"
+        nmcli con down "$adapter"
+        nmcli con up "$adapter"
+        echo "Primary DNS: $primary_dns, Secondary DNS: $secondary_dns"
+    fi
+fi
